fa65be01c82b2687cb3193a3aa7a79f00d50684d
Zaid Khan <zaidkhann0129@gmail.com>
2025-05-19 13:14:46 +0530

    commit

--- index.js
+++ index.js
@@ -21,16 +21,49 @@
 // })
 
 
+// STUDENTS 
+
+// let express = require ("express");
+// let app = express();
+// let mongoose = require ("mongoose");
+// let stuRoutes = require("./Routes/stuRoutes");
+// let bodyParser = require('body-parser');
+
+// mongoose.connect("mongodb://localhost:27017/zaiddatabase").then(()=>{
+//     console.log("MongoDB Successfully Connected ")
+// })
+
+
+// Parse incoming requests with JSON payloads
+// app.use(bodyParser.json());
+
+// Parse incoming requests with urlencoded payloads
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+// app.set("view engine", "ejs");
+// app.use("/student", stuRoutes);
+
+
+
+
+// app.listen(9000, ()=>{
+//     console.log("Server run on 9000")
+// })
+
+
+// -------------------------------------------EMPLOYESS DATE- 19/05/2025
+
 let express = require ("express");
 let app = express();
-let mongoose = require ("mongoose");
-let stuRoutes = require("./Routes/stuRoutes");
-let bodyParser = require('body-parser');
-
-mongoose.connect("mongodb://localhost:27017/zaiddatabase").then(()=>{
-    console.log("MongoDB Successfully Connected ")
+let EmpRoute = require("./Routes/empRoute");
+let mongoose = require("mongoose");
+let bodyParser = require('body-parser')
+mongoose.connect("mongodb://localhost:27017/khandb").then(()=>{
+    console.log("DB Connected Succesfully!!!")
 })
-
 
 // Parse incoming requests with JSON payloads
 app.use(bodyParser.json());
@@ -38,15 +71,10 @@ app.use(bodyParser.json());
 // Parse incoming requests with urlencoded payloads
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Set EJS as templating engine
 
-
-
-app.set("view engine", "ejs");
-app.use("/student", stuRoutes);
-
-
-
-
-app.listen(9000, ()=>{
-    console.log("Server run on 9000")
+app.set('view engine', 'ejs');
+app.use("/", EmpRoute);
+app.listen(9000,()=>{
+    console.log("Server run on 9000 port!!!")
 })
